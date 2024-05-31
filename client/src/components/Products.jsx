ae2935beefb824dc8ddc84928a4c7926f96a00a0
mokshitpuri <mokshitp44@gmail.com>
2024-05-31 15:28:07 +0530

    committed

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -65,7 +65,7 @@ const Products = ({cat, filters, sort}) => {
       {cat ? filteredProducts.map((item)=>(
         <Product item={item} key={item.id}/>
       )) : products
-        .slice(0,8)
+        // .slice(0,8)
         .map((item)=> <Product item={item} key={item.id}/>
       )}
     </Container>
